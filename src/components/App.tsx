d62e9a1897ddefd885be778d0340d7c520f9b868
Huy Nguyen <huyn.nguyen95@gmail.com>
2022-09-02 10:47:37 -0500

    styling component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,41 @@
+import ProdDesk from '../assets/images/image-product-desktop.jpg'
+import ProdMob from '../assets/images/image-product-mobile.jpg'
+import Cart from '../assets/images/icon-cart.svg'
+
 function App() {
   return (
-    <div>Ready</div>
+    <div className="grid h-screen w-full place-content-center bg-cream">
+      <div className="mx-6 overflow-hidden rounded-xl md:flex md:max-h-image md:max-w-4xl md:justify-center">
+        <section className="md:w-1/2">
+          <picture>
+            <source media="(min-width:768px)" srcSet={ProdDesk} />
+            <img className="h-full w-full" src={ProdMob} alt="chanel perfume" />
+          </picture>
+        </section>
+        <section className="bg-white font-montserrat font-medium text-grayishBlue md:w-1/2">
+          <div className="space-y-5 px-7 py-7 md:flex md:h-full md:flex-col md:justify-around md:space-y-0 md:px-13">
+            <span className="text-lg uppercase tracking-xl">Perfume</span>
+            <h1 className="font-fraunces text-title font-bold leading-none text-darkBlue md:text-5xl">
+              Gabrielle Essence Eau De Parfum
+            </h1>
+            <p className="text-paragraph leading-8 md:text-xl">
+              A floral, solar and voluptuous interpretation composed by Olivier
+              Polge, Perfumer-Creator for the House of CHANEL.
+            </p>
+            <div className="flex items-center">
+              <h1 className="font-fraunces text-title font-bold text-darkCyan">
+                $149.99
+              </h1>
+              <span className="ml-7 text-xl line-through">$169.99</span>
+            </div>
+            <button className="mx-auto flex w-full items-center justify-center rounded-lg bg-darkCyan py-5 text-xl font-bold text-white">
+              <img className="mr-4 h-6" src={Cart} alt="cart icon" />
+              Add to Cart
+            </button>
+          </div>
+        </section>
+      </div>
+    </div>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,51 @@
 
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  screen: {
+    sm: '375px',
+    md: '768px',
+    lg: '1024px',
+    xl: '1440px'
+  },
   theme: {
-    extend: {}
+    fontSize: {
+      xs: '.75rem',
+      sm: '.875rem',
+      tiny: '.875rem',
+      base: '1rem',
+      lg: '1.125rem',
+      xl: '1.25rem',
+      '2xl': '1.5rem',
+      '3xl': '1.875rem',
+      '4xl': '2.25rem',
+      '5xl': '3rem',
+      '6xl': '4rem',
+      '7xl': '5rem',
+      title: '2.75rem',
+      paragraph: '1.185rem'
+    },
+    fontFamily: {
+      fraunces: ['Fraunces', 'serif'],
+      montserrat: ['Montserrat', 'sans-serif']
+    },
+    letterSpacing: {
+      xl: '.5rem'
+    },
+    extend: {
+      padding: {
+        '13': '3.1rem'
+      },
+      maxHeight: {
+        image: '40rem'
+      },
+      colors: {
+        darkCyan: 'hsl(158, 36%, 37%)',
+        cream: 'hsl(30, 38%, 92%)',
+        darkBlue: 'hsl(212, 21%, 14%)',
+        grayishBlue: 'hsl(228, 12%, 48%)',
+        white: 'hsl(0, 0%, 100%)'
+      }
+    }
   },
   plugins: []
 }
